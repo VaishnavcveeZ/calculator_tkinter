84d0522b63011ba0b545077ba28c8125f3e583a5
vaishnav cv <vaishnavcveez@gmail.com>
2020-06-27 19:03:16 +0530

    butten text bold and allignment adjusted

--- calculator.py
+++ calculator.py
@@ -1,46 +1,55 @@
 from tkinter import *
+import tkinter.font as font
+
 window=Tk()
 window.configure(bg='#22324e')
 window.title('CALCULATOR')
 l=Label(window,text='CALCULATOR')
 l.grid()
-screen=Entry(window, bg='#0000ff',fg='#F4D03F',width=60,borderwidth=5)
+
+myfont=font.Font(family='Helvetica')
+bfont=font.Font(size=15,weight='bold')
+
+screen=Entry(window, font=myfont,bg='#0000ff',fg='#F4D03F',width=50,borderwidth=5)
 screen.grid(row=1,column=0,columnspan=4,padx=10,pady=10,ipady=15)
+
+
 
 def button_click(num) :
 
     screen.insert(0,num)
     print('button clicked',num)
 
-x=40
-y=20
-w1=5
-w2=3
+x=50 #padx of to keys
+y=10 #pady of to keys
+w1=5#border width of to line keys
+w2=5 #border width of other keys
 
-button_CLR=Button(window,text='CLR' ,borderwidth=w1 ,padx=x,pady=y,bg='#666699',command=lambda: button_click('c')).grid(row=3 ,column=0)
-button_MOD=Button(window,text='%' ,borderwidth=w1,padx=x,pady=y,bg='#666699',command=lambda: button_click('%')).grid(row= 3,column=1)
-button_ON=Button(window,text='DEL' ,borderwidth=w1,padx=x,pady=y,bg='#666699',command=lambda: button_click('del')).grid(row= 3,column=2)
-button_OFF=Button(window,text='ON/OFF' ,borderwidth=w1,padx=x,pady=y,bg='#666699',fg='white',command=lambda: button_click('o')).grid(row=3 ,column=3)
 
-button_7=Button(window,text='7' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(7)).grid(row= 4,column=0)
-button_8=Button(window,text='8' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(8)).grid(row= 4,column=1)
-button_9=Button(window,text='9' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(9)).grid(row= 4,column=2)
-button_ADD=Button(window,text='+' ,borderwidth=w1,padx=50,pady=y,bg='#666699',command=lambda: button_click('+')).grid(row=4 ,column=3)
+button_CLR=Button(window,text='CLR' ,font=bfont,borderwidth=w1 ,padx=45,pady=y,bg='#666699',command=lambda: button_click('c')).grid(row=3 ,column=0)
+button_MOD=Button(window,text='%' ,font=bfont,borderwidth=w1,padx=45,pady=y,bg='#666699',command=lambda: button_click('%')).grid(row= 3,column=1)
+button_ON=Button(window,text='DEL' ,font=bfont,borderwidth=w1,padx=45,pady=y,bg='#666699',command=lambda: button_click('del')).grid(row= 3,column=2)
+button_OFF=Button(window,text='ON/OFF' ,font=bfont,borderwidth=w1,padx=35,pady=y,bg='#666699',fg='#ffffff',command=lambda: button_click('o')).grid(row=3 ,column=3)
 
-button_4=Button(window,text='4' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(4)).grid(row= 5,column=0)
-button_5=Button(window,text='5' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(5)).grid(row= 5,column=1)
-button_6=Button(window,text='6' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(6)).grid(row= 5,column=2)
-button_SUB=Button(window,text='-' ,borderwidth=w1,padx=50,pady=y,bg='#666699',command=lambda: button_click('-')).grid(row=5 ,column=3)
+button_7=Button(window,text='7' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(7)).grid(row= 4,column=0)
+button_8=Button(window,text='8' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(8)).grid(row= 4,column=1)
+button_9=Button(window,text='9' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(9)).grid(row= 4,column=2)
+button_ADD=Button(window,text='+' ,font=bfont,borderwidth=w2,padx=67,pady=y,bg='#666699',command=lambda: button_click('+')).grid(row=4 ,column=3)
 
-button_1=Button(window,text='1' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(1)).grid(row= 6,column=0)
-button_2=Button(window,text='2' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(2)).grid(row= 6,column=1)
-button_3=Button(window,text='3' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(3)).grid(row= 6,column=2)
-button_MUL=Button(window,text='x' ,borderwidth=w1,padx=50,pady=y,bg='#666699',command=lambda: button_click('x')).grid(row=6 ,column=3)
+button_4=Button(window,text='4' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(4)).grid(row= 5,column=0)
+button_5=Button(window,text='5' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(5)).grid(row= 5,column=1)
+button_6=Button(window,text='6' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(6)).grid(row= 5,column=2)
+button_SUB=Button(window,text='-' ,font=bfont,borderwidth=w1,padx=69,pady=y,bg='#666699',command=lambda: button_click('-')).grid(row=5 ,column=3)
 
-button_DOT=Button(window,text='.' ,borderwidth=w2,padx=x,pady=y,bg='#666699',command=lambda: button_click('.')).grid(row= 7,column=0)
-button_0=Button(window,text='0' ,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(0)).grid(row= 7,column=1)
-button_EQL=Button(window,text='=' ,borderwidth=w2,padx=x,pady=y,bg='#eeeeee',command=lambda: button_click('=')).grid(row=7 ,column=2)
-button_DIV=Button(window,text='/' ,borderwidth=w1,padx=50,pady=y,bg='#666699',command=lambda: button_click('/')).grid(row= 7,column=3)
+button_1=Button(window,text='1' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(1)).grid(row= 6,column=0)
+button_2=Button(window,text='2' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(2)).grid(row= 6,column=1)
+button_3=Button(window,text='3' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(3)).grid(row= 6,column=2)
+button_MUL=Button(window,text='x' ,font=bfont,borderwidth=w1,padx=67,pady=y,bg='#666699',command=lambda: button_click('x')).grid(row=6 ,column=3)
+
+button_DOT=Button(window,text='.' ,font=bfont,borderwidth=w2,padx=51,pady=y,bg='#666699',command=lambda: button_click('.')).grid(row= 7,column=0)
+button_0=Button(window,text='0' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#B22222',command=lambda: button_click(0)).grid(row= 7,column=1)
+button_EQL=Button(window,text='=' ,font=bfont,borderwidth=w2,padx=x,pady=y,bg='#eeeeee',command=lambda: button_click('=')).grid(row=7 ,column=2)
+button_DIV=Button(window,text='/' ,font=bfont,borderwidth=w1,padx=69,pady=y,bg='#666699',command=lambda: button_click('/')).grid(row= 7,column=3)
 
 
 
