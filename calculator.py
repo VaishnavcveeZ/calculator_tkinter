1ba2f915d19f9fe9092c76f42ded30730acc4e91
vaishnav cv <vaishnavcveez@gmail.com>
2020-06-27 16:29:09 +0530

    one button added

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 from tkinter import *
+window=Tk()
+window.geometry('400x600')
+l=Label(window,text='CALCULATOR').pack()
+screen=Entry(window, width=60,)
+screen.pack()
+
+button=Button(window,text='' ,padx=40,pady=20,command=button_click).grid(row= ,column=)
+
+
+
+
+
+
+window.mainloop()
